2d84af6b31e35a07daab3e77b125706a0d1031f1
atomimox <atomimox05stalker@gmail.com>
2024-10-18 21:35:10 -0400

    BD actualizada

--- proyecto.sql
+++ proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-09-2024 a las 21:27:33
+-- Tiempo de generación: 19-10-2024 a las 03:33:42
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -40,6 +40,29 @@ CREATE TABLE `departamento` (
 INSERT INTO `departamento` (`id`, `name`, `responsable_dep`) VALUES
 (1, 'Tecnología', 'Administrador'),
 (2, 'Departamento de bienes y almacén', 'Maria Vivas');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `id` int(11) NOT NULL,
+  `ci` varchar(8) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `cargo` varchar(50) NOT NULL,
+  `departamento` int(11) NOT NULL,
+  `activo` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`id`, `ci`, `nombre`, `apellido`, `cargo`, `departamento`, `activo`) VALUES
+(1, '14852799', 'Omar', 'Rivero', 'Auditor', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -100,7 +123,6 @@ CREATE TABLE `movimientos` (
   `usuario` int(11) NOT NULL,
   `type` tinyint(4) NOT NULL,
   `funcionario` varchar(80) NOT NULL,
-  `cargo` varchar(50) NOT NULL,
   `motivo` varchar(80) NOT NULL,
   `date` datetime NOT NULL DEFAULT current_timestamp(),
   `observaciones` varchar(255) NOT NULL,
@@ -112,13 +134,13 @@ CREATE TABLE `movimientos` (
 -- Volcado de datos para la tabla `movimientos`
 --
 
-INSERT INTO `movimientos` (`id`, `equipo`, `usuario`, `type`, `funcionario`, `cargo`, `motivo`, `date`, `observaciones`, `n_control`, `defeated`) VALUES
-(2, 3, 10, 0, 'Diego', 'Coordinador', 'Auditoria', '2024-09-11 09:47:37', 'Se entrega en buenas condiciones', '66e19f7918bfe', 0),
-(3, 2, 10, 0, 'Pepe', 'Coordinador', 'Auditoria', '2024-09-11 10:29:29', 'Sin observaciones', '66e1a94931984', 0),
-(4, 3, 10, 1, 'Jorge', 'Coordinador', 'Auditoria', '2024-09-11 10:58:09', 'Se recibe en buenas condiciones', NULL, 1),
-(5, 4, 10, 0, 'Omar Rivero', 'Abogado Auditor', 'Jornada de auditoría', '2024-09-12 19:46:59', 'Se entrega en excelentes condiciones', '66e37d7330e33', 0),
-(6, 2, 10, 1, 'Pepe', 'Auditor', 'Auditoria', '2024-09-12 19:52:59', 'S/O', NULL, 1),
-(7, 4, 14, 1, 'Omar Rivero', 'Auditor', 'Jornada de auditoría', '2024-09-17 11:13:41', 'Sin observaciones, entrega en buen estado', NULL, 1);
+INSERT INTO `movimientos` (`id`, `equipo`, `usuario`, `type`, `funcionario`, `motivo`, `date`, `observaciones`, `n_control`, `defeated`) VALUES
+(2, 3, 10, 0, 'Diego', 'Auditoria', '2024-09-11 09:47:37', 'Se entrega en buenas condiciones', '66e19f7918bfe', 0),
+(3, 2, 10, 0, 'Pepe', 'Auditoria', '2024-09-11 10:29:29', 'Sin observaciones', '66e1a94931984', 0),
+(4, 3, 10, 1, 'Jorge', 'Auditoria', '2024-09-11 10:58:09', 'Se recibe en buenas condiciones', NULL, 1),
+(5, 4, 10, 0, 'Omar Rivero', 'Jornada de auditoría', '2024-09-12 19:46:59', 'Se entrega en excelentes condiciones', '66e37d7330e33', 0),
+(6, 2, 10, 1, 'Pepe', 'Auditoria', '2024-09-12 19:52:59', 'S/O', NULL, 1),
+(7, 4, 14, 1, 'Omar Rivero', 'Jornada de auditoría', '2024-09-17 11:13:41', 'Sin observaciones, entrega en buen estado', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -131,7 +153,6 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(40) NOT NULL,
   `apellido` varchar(40) NOT NULL,
   `ci` varchar(8) NOT NULL,
-  `n_carnet` varchar(7) NOT NULL,
   `departamento` int(11) NOT NULL,
   `rol` tinyint(4) NOT NULL,
   `estatus` tinyint(4) NOT NULL,
@@ -143,12 +164,11 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `ci`, `n_carnet`, `departamento`, `rol`, `estatus`, `username`, `password`) VALUES
-(10, 'Derek', 'Ramos', '29751122', '8989898', 1, 0, 0, 'derek', '$2y$10$QfY8ncSkh4Lfm9rhOjNuT.PzR0O5lkcRe9EZHfOyE5AzNvysg/WAi'),
-(11, 'prueba', 'deshabilitar', '79879779', '7987779', 1, 2, 1, 'editado', '$2y$10$gPXp.bkY9/7iVPI415/wp.1RVYNXg/gc53FP4sHFcTwNlXpeI8uKi'),
-(12, 'Prueba', 'prueba', '89989789', '7899898', 1, 3, 1, 'prueba', '$2y$10$e90x14QL66UBDDpUbzpOT.1JBSidxEA..9Tn2dDwjRhju4SEniJQe'),
-(13, 'José', 'Peña', '18000000', '123456', 1, 0, 0, 'peña', '$2y$10$cy7U9u5PjN993XtOjPq1Zeu0tRQLJ7timGC1a21pJFj7YVLo3vjjK'),
-(14, 'Analista', 'Tecnologia', '56565656', '8989898', 1, 1, 0, 'analista', '$2y$10$1Ee6eSHOlHcVW8Wfq82U9ujC2spNIJ3IGwlsbqWMvFgSnpXMxhOwa');
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `ci`, `departamento`, `rol`, `estatus`, `username`, `password`) VALUES
+(10, 'Derek', 'Ramos', '29751122', 1, 0, 0, 'derek', '$2y$10$QfY8ncSkh4Lfm9rhOjNuT.PzR0O5lkcRe9EZHfOyE5AzNvysg/WAi'),
+(11, 'prueba', 'deshabilitar', '79879779', 1, 2, 1, 'editado', '$2y$10$gPXp.bkY9/7iVPI415/wp.1RVYNXg/gc53FP4sHFcTwNlXpeI8uKi'),
+(13, 'José', 'Peña', '18000000', 1, 0, 0, 'peña', '$2y$10$cy7U9u5PjN993XtOjPq1Zeu0tRQLJ7timGC1a21pJFj7YVLo3vjjK'),
+(14, 'Analista', 'Tecnologia', '56565656', 1, 1, 0, 'analista', '$2y$10$1Ee6eSHOlHcVW8Wfq82U9ujC2spNIJ3IGwlsbqWMvFgSnpXMxhOwa');
 
 --
 -- Índices para tablas volcadas
@@ -159,6 +179,13 @@ INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `ci`, `n_carnet`, `departame
 --
 ALTER TABLE `departamento`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `departamento` (`departamento`);
 
 --
 -- Indices de la tabla `equipos`
@@ -201,6 +228,12 @@ ALTER TABLE `departamento`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `equipos`
 --
 ALTER TABLE `equipos`
@@ -227,6 +260,12 @@ ALTER TABLE `usuarios`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD CONSTRAINT `empleados_ibfk_1` FOREIGN KEY (`departamento`) REFERENCES `departamento` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `mantenimientos`
