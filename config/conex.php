99f1b0bbaa45c6ea9ae279675cf69cac15f2274c
atomimox <atomimox05stalker@gmail.com>
2024-12-03 09:45:24 -0400

    Final.

--- config/conex.php
+++ config/conex.php
@@ -4,12 +4,6 @@
     $pass = "";
     $db = "proyecto";
 
-    // $host = "localhost";
-    // $user = "codejodc_derek";
-    // $pass = "Py1thon.23";
-    // $db = "codejodc_proyecto";
-
-
     $conn = new mysqli($host, $user, $pass, $db);
 
     if(!$conn){
